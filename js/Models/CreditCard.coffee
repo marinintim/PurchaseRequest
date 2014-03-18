6178d1081aac51b72d66c62b3a8237271f6cb78e
Marinin Tim <marinintim@gmail.com>
2014-03-18 18:18:22 +0400

    Bugfix #45830 #45831: Credit Card validation
    We discussed with Sergey what properly credit card validation is not needed (is it properly VISA or MasterCard), so now validation checks for numeric type only.
    
    Also, credit cards that were given by server now are not validated.

--- js/Models/CreditCard.coffee
+++ js/Models/CreditCard.coffee
@@ -10,21 +10,24 @@ define "Models/CreditCard",
 
 		validate: (attributes) ->
 			attributes ?= @attributes
+			if this.isNew()
+				return "Credit card fields are not filled" if _.isEmpty attributes
+				return "Credit card: card number is empty" if _.isEmpty attributes.number
+				return "Credit card: cardholder is empty" if _.isEmpty attributes.card_holder
+				return "Credit card: expiration date is incomplete" if _.isEmpty attributes.expiration_month or _.isEmpty attributes.expiration_year
 
-			return "Credit card fields are not filled" if _.isEmpty attributes
-			return "Credit card: expiration date is incomplete" if _.isEmpty attributes.expiration_month or _.isEmpty attributes.expiration_year
-			return "Credit card: card number is empty" if _.isEmpty attributes.number
-			return "Credit card: cardholder is empty" if _.isEmpty attributes.card_holder
+				return "Credit card: invalid number" unless /^[0-9]+$/.test attributes.number.replace /[\s-\\\/]+/g, ""
+				return "Credit card: invalid expiration month" unless $.isNumeric attributes.expiration_month
+				return "Credit card: invalid expiration year" unless $.isNumeric attributes.expiration_year
 
-			
-			number = parseInt(@attributes.number.split(" ").join(""),10)
-
-
-			expire_date = new Date("#{attributes.expiration_month}/1/#{attributes.expiration_year}")
-			expire_date.setMonth expire_date.getMonth + 1
-
-			today_date = new Date()
-
-			return "Already expired credit card!" if expire_date - today_date <= 0
+				expire_date = new Date()
+				expire_date.setDate 1
+				expire_date.setFullYear 2000 + parseInt attributes.expiration_year, 10
+				expire_date.setMonth + parseInt attributes.expiration_month, 10
+				
+				today_date = new Date()
+				window.expire = expire_date
+				console.log expire_date - today_date
+				return "Credit card is already expired" if expire_date - today_date <= 0
 
 	return CreditCard
--- js/Models/CreditCard.js
+++ js/Models/CreditCard.js
@@ -11,28 +11,42 @@
         expiration_year: ""
       },
       validate: function(attributes) {
-        var expire_date, number, today_date;
+        var expire_date, today_date;
         if (attributes == null) {
           attributes = this.attributes;
         }
-        if (_.isEmpty(attributes)) {
-          return "Credit card fields are not filled";
-        }
-        if (_.isEmpty(attributes.expiration_month || _.isEmpty(attributes.expiration_year))) {
-          return "Credit card: expiration date is incomplete";
-        }
-        if (_.isEmpty(attributes.number)) {
-          return "Credit card: card number is empty";
-        }
-        if (_.isEmpty(attributes.card_holder)) {
-          return "Credit card: cardholder is empty";
-        }
-        number = parseInt(this.attributes.number.split(" ").join(""), 10);
-        expire_date = new Date("" + attributes.expiration_month + "/1/" + attributes.expiration_year);
-        expire_date.setMonth(expire_date.getMonth + 1);
-        today_date = new Date();
-        if (expire_date - today_date <= 0) {
-          return "Already expired credit card!";
+        if (this.isNew()) {
+          if (_.isEmpty(attributes)) {
+            return "Credit card fields are not filled";
+          }
+          if (_.isEmpty(attributes.number)) {
+            return "Credit card: card number is empty";
+          }
+          if (_.isEmpty(attributes.card_holder)) {
+            return "Credit card: cardholder is empty";
+          }
+          if (_.isEmpty(attributes.expiration_month || _.isEmpty(attributes.expiration_year))) {
+            return "Credit card: expiration date is incomplete";
+          }
+          if (!/^[0-9]+$/.test(attributes.number.replace(/[\s-\\\/]+/g, ""))) {
+            return "Credit card: invalid number";
+          }
+          if (!$.isNumeric(attributes.expiration_month)) {
+            return "Credit card: invalid expiration month";
+          }
+          if (!$.isNumeric(attributes.expiration_year)) {
+            return "Credit card: invalid expiration year";
+          }
+          expire_date = new Date();
+          expire_date.setDate(1);
+          expire_date.setFullYear(2000 + parseInt(attributes.expiration_year, 10));
+          expire_date.setMonth + parseInt(attributes.expiration_month, 10);
+          today_date = new Date();
+          window.expire = expire_date;
+          console.log(expire_date - today_date);
+          if (expire_date - today_date <= 0) {
+            return "Credit card is already expired";
+          }
         }
       }
     });
